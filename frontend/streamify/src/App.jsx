6601864b4a30be3fbc49bbe1785abaeec76877c4
DebarshiMondal1055 <rishimondal99@gmail.com>
2025-07-25 13:06:00 +0530

    routes creating

--- frontend/streamify/src/App.jsx
+++ frontend/streamify/src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage.jsx'
+import LoginPage from './Pages/LoginPage.jsx'
+import SignUpPage from './Pages/SignUpPage.jsx'
+import ChatPage from './Pages/ChatPage.jsx'
+import NotificationsPage from './Pages/notificationsPage.jsx'
+import OnBoardingPage from './Pages/onBoardingPage.jsx'
+import CallPage from './Pages/CallPage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen' data-theme="coffee">
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signUp' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/on-board' element={<OnBoardingPage/>}/>
+        <Route path='/notifications' element={<NotificationsPage/>}/>
+        <Route path='/chats' element={<ChatPage/>}/>
+        <Route path='/calls' element={<CallPage/>}/>
+
+      </Routes>
+    </div>
   )
 }
 
